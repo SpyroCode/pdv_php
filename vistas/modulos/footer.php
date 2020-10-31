679056cbec01637efebc111b0a8d13b7adb644e9
Efren Almanza Lamas <37811579+SpyroCode@users.noreply.github.com>
2020-10-31 13:22:04 -0600

    [ADD]Change footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2017 <a href="https://www.tutorialesatualcance.com" target="_blank">Tutoriales a tu Alcance</a>.</strong>
+	<strong>Copyright &copy; <?=date('Y')?> <a href="https://www.spyrocode.com" target="_blank">SpyroCode</a>.</strong>
 
 	Todos los derechos reservados.
 
